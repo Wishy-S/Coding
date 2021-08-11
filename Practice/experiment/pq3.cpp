80176bfe0ba023609c9b12ddfc15f436e22ed3ce
Wishy-S <mk2371972@gmail.com>
2021-08-11 12:22:37 +0530

    priority_queue_variants added

---
+++ Practice/experiment/pq3.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define all(v) v.begin(),v.end()
+#define pb(x) push_back(x)
+#define f first
+#define s second
+#define vt vector
+#define boost ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);  
+const ll mod = 1e9+7;
+const int d4i[4]={-1, 0, 1, 0}, d4j[4]={0, 1, 0, -1};
+const int d8i[8]={-1, -1, 0, 1, 1, 1, 0, -1}, d8j[8]={0, 1, 1, 1, 0, -1, -1, -1};
+
+//here implementation begins 
+//if u want to pass a custom comparator to PQ;
+//then use a structure and pass it to the PQ while declaring as shown below 
+struct cmp{
+	bool operator()(const int &a,const int &b){
+		return a > b;
+	}
+};
+void solve(){
+	int n;cin>>n;
+
+	priority_queue<int,vector<int>,cmp>pq;
+	for(int i = 0;i<n;i++){
+		int x;cin>>x;
+		pq.push(x);
+	}
+	cout<<pq.top()<<endl;
+
+}
+int main()
+{	boost;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	t = 1;
+	// cin>>t;
+	//cin>>t>>n>>q;
+	//clock_t clk = clock();
+	for(int i = 0;i<t;i++){
+		//cout<<"Case #"<<i+1<<": ";
+		solve();
+		}
+	
+	//clk = clock()-clk;
+	//cout<<"TIME CONSUMED: "<<(float)(clk)/CLOCKS_PER_SEC<<"sec\n";
+}
